13090c115bafe3b72b5a5de7462f9fc4a3f901da
hong <h@123.com>
2025-01-05 21:53:06 +0800

    detail国际化

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,15 +7,15 @@ export const getHeaderData = (lang: string = 'zh') => ({
       links: [
         {
           text: lang === 'zh' ? '静态住宅代理' : 'Static Residential Proxy',
-          href: `/${lang}/detail`,
+          href: `/${lang}/detail#static-residential`,
         },
         {
           text: lang === 'zh' ? '动态住宅代理' : 'Dynamic Residential Proxy',
-          href: `/${lang}/detail`,
+          href: `/${lang}/detail#dynamic-residential`,
         },
         {
           text: lang === 'zh' ? '数据中心代理' : 'Data Center Proxy',
-          href: `/${lang}/detail`,
+          href: `/${lang}/detail#datacenter`,
         },
       ],
     },
