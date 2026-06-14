d4572c62c9c5784b3a93c8695bfb2269438e46cb
hzx <johndoe@example.com>
2026-06-14 22:29:17 +0800

    refactor: organize utils barrel exports by category

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,21 @@
-export { useTranslations, initializeTranslations, getLanguageFromURL } from './i18n';
-export { getStaticPathsForLang, generateBreadcrumbSchema, generateFAQSchema } from './schema';
+// Core utilities
 export { formatDate, slugify, truncateText, generateReadingTime } from './utils';
+export { getContentPaths, ensureDirectory, copyFiles, cleanupTemp } from './directories';
+
+// Internationalization
+export { useTranslations, initializeTranslations, getLanguageFromURL } from './i18n';
+
+// Images
 export { getImage, getOptimizedImageSrc, generateImageSizes } from './images';
+
+// Content processing
+export { getBlogList, getBlogPermalink } from './blog';
+export { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './frontmatter';
+
+// SEO & URLs
 export { getPermalink, getHomePermalink, getHomePermalinkEn, getHomePermalinkZh, trimSlash, getAsset } from './permalinks';
+export { getStaticPathsForLang, generateBreadcrumbSchema, generateFAQSchema } from './schema';
+export type { SitemapEntry } from './seo-optimization';
+
+// Cache
+export { cacheParams, getCachedParams } from './paramCache';
